0e3d2de112ec77381927f1fab2aa3bf36e613abc
Dorottya Bernula-Orosz <bernula-orosz.dorottya@fits.hu>
2024-04-03 10:17:40 +0200

    changed actual and expected :)

--- UnitTestingHomework/CalculatorTests.cs
+++ UnitTestingHomework/CalculatorTests.cs
@@ -13,34 +13,34 @@ namespace UnitTestingHomework
                 var expected = 5;
                 var actual = Calculator.Add(2, 3);
 
-                Assert.Equal(expected,actual);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestAddingNegativeIntegers()
             {
-                var actual = -1;
-                var expected = Calculator.Add(-2, 1);
+                var expected = -1;
+                var actual = Calculator.Add(-2, 1);
 
-                Assert.Equal(actual,expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestAddingZeroToPositiveInteger()
             {
-                var actual = 5;
-                var expected = Calculator.Add(5, 0);
+                var expected = 5;
+                var actual = Calculator.Add(5, 0);
 
-                Assert.Equal(actual,expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestAddingFloatingPointNumbers()
             {
-                var actual = 4.5;
-                var expected = Calculator.Add(2.5, 2);
+                var expected = 4.5;
+                var actual = Calculator.Add(2.5, 2);
 
-                Assert.Equal(actual,expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
@@ -51,8 +51,10 @@ namespace UnitTestingHomework
                 var actual = Calculator.Add(a, b);
                 var expected = Calculator.Add(b, a);
 
-                Assert.Equal(actual,expected);
+                Assert.Equal(expected, actual);
+
             }
+
         }
 
         public class SubtractTests
@@ -60,57 +62,55 @@ namespace UnitTestingHomework
             [Fact]
             public void TestSubtractingPositiveIntegers()
             {
-                var actual = 1;
-                var expected = Calculator.Subtract(3, 2);
+                var expected = 1;
+                var actual = Calculator.Subtract(3, 2);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestSubtractingNegativeIntegers()
             {
-                var actual = -5;
-                var expected = Calculator.Subtract(-1, 4);
+                var expected = -5;
+                var actual = Calculator.Subtract(-1, 4);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestSubtractingZeroFromPositiveInteger()
             {
-                var actual = 5;
-                var expected = Calculator.Subtract(5,0);
+                var expected = 5;
+                var actual = Calculator.Subtract(5, 0);
 
-                // Assert
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestSubtractingFloatingPointNumbers()
             {
-                var actual = 1.5;
-                var expected = Calculator.Subtract(3.5, 2);
+                var expected = 1.5;
+                var actual = Calculator.Subtract(3.5, 2);
 
-                // Assert
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestXLessThanY()
             {
-                var actual = -2;
-                var expected = Calculator.Subtract(2, 4);
+                var expected = -2;
+                var actual = Calculator.Subtract(2, 4);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestYLessThanX()
             {
-                var actual = 2;
-                var expected = Calculator.Subtract(4, 2);
+                var expected = 2;
+                var actual = Calculator.Subtract(4, 2);
 
-                Assert.Equal(actual,expected);
+                Assert.Equal(expected, actual);
             }
         }
 
@@ -119,37 +119,37 @@ namespace UnitTestingHomework
             [Fact]
             public void TestMultiplyingPositiveIntegers()
             {
-                var actual = 6;
-                var expected = Calculator.Multiply(2, 3);
+                var expected = 6;
+                var actual = Calculator.Multiply(2, 3);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestMultiplyingNegativeIntegers()
             {
-                var actual = -6;
-                var expected = Calculator.Multiply(-2, 3);
+                var expected = -6;
+                var actual = Calculator.Multiply(-2, 3);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestMultiplyingAnIntegerWithZero()
             {
-                var actual = 0;
-                var expected = Calculator.Multiply(5, 0);
+                var expected = 0;
+                var actual = Calculator.Multiply(5, 0);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestMultiplyingFloatingPointNumbers()
             {
-                var actual = 6.75;
-                var expected = Calculator.Multiply(2.25, 3);
+                var expected = 6.75;
+                var actual = Calculator.Multiply(2.25, 3);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
@@ -157,10 +157,10 @@ namespace UnitTestingHomework
             {
                 double a = 3, b = 5;
 
-                var actual = Calculator.Multiply(a, b);
-                var expected = Calculator.Multiply(b, a);
+                var expected = Calculator.Multiply(a, b);
+                var actual = Calculator.Multiply(b, a);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
         }
 
@@ -169,43 +169,43 @@ namespace UnitTestingHomework
             [Fact]
             public void TestDividingPositiveIntegers()
             {
-                var actual = 2;
-                var expected = Calculator.Divide(6, 3);
+                var expected = 2;
+                var actual = Calculator.Divide(6, 3);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestDividingNegativeIntegers()
             {
-                var actual = -2;
-                var expected = Calculator.Divide(-6, 3);
+                var expected = -2;
+                var actual = Calculator.Divide(-6, 3);
 
-                Assert.Equal(actual,expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestZeroDividendThrowingError()
-            {              
+            {
                 Assert.Throws<DivideByZeroException>(() => Calculator.Divide(5, 0));
             }
 
             [Fact]
             public void TestZeroDivisor()
             {
-                var actual = 0;
-                var expected = Calculator.Divide(0, 5);
+                var expected = 0;
+                var actual = Calculator.Divide(0, 5);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestDividingFloatingPointNumbers()
             {
-                var actual = 2.5;
-                var expect4ed = Calculator.Divide(5, 2);
+                var expected = 2.5;
+                var actual = Calculator.Divide(5, 2);
 
-                Assert.Equal(actual, expect4ed);
+                Assert.Equal(expected, actual);
             }
         }
 
@@ -214,25 +214,24 @@ namespace UnitTestingHomework
             [Fact]
             public void TestSquareRootingPositiveNumber()
             {
-                var actual = 2;
-                var expected = Calculator.SquareRoot(4);
+                var expected = 2;
+                var actual = Calculator.SquareRoot(4);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestSquareRootingZero()
             {
+                var expected = 0;
+                var actual = Calculator.SquareRoot(0);
 
-                var actual = 0;
-                var expected = Calculator.SquareRoot(0);
-
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestSquareRootingNegativeNumberThrowingError()
-            {                
+            {
                 Assert.Throws<ArgumentException>(() => Calculator.SquareRoot(-4));
             }
         }
@@ -242,37 +241,37 @@ namespace UnitTestingHomework
             [Fact]
             public void TestPoweringPositiveIntegers()
             {
-                var actual = 8;
-                var expected = Calculator.Power(2, 3);
+                var expected = 8;
+                var actual = Calculator.Power(2, 3);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestPoweringNegativeIntegers()
             {
-                var actual = -8;
-                var expected = Calculator.Power(-2, 3);
+                var expected = -8;
+                var actual = Calculator.Power(-2, 3);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestPoweringZeroExponent()
             {
-                var actual = 1;
-                var expected = Calculator.Power(5, 0);
+                var expected = 1;
+                var actual = Calculator.Power(5, 0);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
 
             [Fact]
             public void TestPoweringFloatingPointNumbers()
             {
-                var actual = 27.0;
-                var expected = Calculator.Power(3.0, 3.0);
+                var expected = 27.0;
+                var actual = Calculator.Power(3.0, 3.0);
 
-                Assert.Equal(actual, expected);
+                Assert.Equal(expected, actual);
             }
         }
     }
